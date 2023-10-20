854cd42b26ee5fb322aa00677cbb73d454e17a08
s4zmobreki <109608950+Arthurmopa@users.noreply.github.com>
2023-10-20 08:28:38 -0300

    Add files via upload

---
+++ aluno/index.js
@@ -0,0 +1,16 @@
+/*Estrutura condicional 
+let idade = 18
+
+if(18 === '18' [] 15 == 15){
+    console.log('bloco 01')
+}else if( 5 >= 5 && <= 6){
+    console.log('bloco 02')
+}else{
+    console.log('bloco 03')
+} */
+
+//estrutura de repetição 
+for(let i = 1; i<= 10; i++){
+    console.log(i)
+}
+console.log(i)
